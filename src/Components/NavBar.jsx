049067e8a21f59b8624163d66baeb41a6ce43219
neleon10 <cvn45@hotmail.com>
2024-04-19 10:05:50 +0200

    tags sematicos agregados

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,23 +3,22 @@ import Image from "next/image";
 
 function NavBar() {
   return (
-    <div>
-      <div>
-        <div>
-          <ul>
-            <li>
-              <Image src="/logo.png" alt="" width="100" height="30" />
-            </li>
-            <li>Home</li>
-            <li>Nosotros</li>
-            <li>Servicios</li>
-            <li>Contacto</li>
-            <li className="">Soy Cliente</li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Image src="/logo.png" alt="" width="100" height="30" />
+          </li>
+          <li>Home</li>
+          <li>Nosotros</li>
+          <li>Servicios</li>
+          <li>Contacto</li>
+          <li className="">Soy Cliente</li>
+        </ul>
+      </nav>
+    </header>
   );
 }
+
 
 export default NavBar;
